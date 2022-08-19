827a1af9243a872914271ec2d00e4066608b6b9b
Sakshi Tripathi <saktripathi@deloitte.com>
2022-08-19 16:08:33 +0530

    adding database model in user-service

--- user_service/config.py
+++ user_service/config.py
@@ -1,8 +1,10 @@
 import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-#for Authorization
-SECRET_KEY = ''
 
-#DB
-SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir,'user.db')
+class Config(object):
+    #for Authorization
+    SECRET_KEY = ''
+
+    #DB
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir,'user.db')
