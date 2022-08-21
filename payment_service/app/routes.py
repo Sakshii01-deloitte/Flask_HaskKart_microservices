e6a71171cf3dc309b682df81d202cb99b8f64ba7
Sakshi Tripathi <saktripathi@deloitte.com>
2022-08-21 16:36:58 +0530

    Item added to the cart

---
+++ payment_service/app/routes.py
@@ -0,0 +1,36 @@
+from app import app, db
+from flask import request, jsonify
+from app.models import Cart, Payment
+import requests
+
+EMPTY_MSG = 'Cart is empty'
+PRODUCT_SERVICE_URL = 'http://127.0.0.1:5001/product/'
+PAYMENT_SERVICE_URL = 'http://127.0.0.1:5002/cart/'
+
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+#add item in your cart
+@app.route('/cart/<int:user_id>/<int:product_id>', methods=['POST'])
+def add_item_to_cart(user_id, product_id):
+    data = request.get_json()
+    product = requests.get(PRODUCT_SERVICE_URL + str(product_id)).json()
+    
+    if data['quantity'] > product['quantity']:
+        return {"message": f"Currently required quantity if {product['product_name']} is not available"},404
+   
+    present_item = Cart.query.filter((Cart.user_id == user_id),(Cart.product_id == product_id)).first()
+    
+    if present_item:
+        present_item.quantity = data['quantity']  
+        db.session.commit()
+        return {"message":"Item added to cart successfully"}
+    
+    item = Cart(user_id=user_id, product_id=product_id, quantity=data['quantity'])
+    db.session.add(item)
+    db.session.commit()
+    return {"message":"Item added to cart successfully"}                   
+    
+    
