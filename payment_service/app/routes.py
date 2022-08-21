1e7f5c61e697ef66ac49f23ab166fec45e96c3a8
Sakshi Tripathi <saktripathi@deloitte.com>
2022-08-21 17:33:21 +0530

    purchase item from the cart

--- payment_service/app/routes.py
+++ payment_service/app/routes.py
@@ -2,6 +2,8 @@ from app import app, db
 from flask import request, jsonify
 from app.models import Cart, Payment
 import requests
+from config import Config
+
 
 EMPTY_MSG = 'Cart is empty'
 PRODUCT_SERVICE_URL = 'http://127.0.0.1:5001/product/'
@@ -79,3 +81,41 @@ def update_cart_item(user_id,product_id):
         db.session.commit()
         return {"message":"Quantity Updated"}
     return {"message":"Item doesn't exists"},404
+
+
+    
+    #Purchase All the items in cart
+@app.route('/buy/<int:user_id>',methods=['POST'])
+def buy_all_cart_items(user_id):
+    try:
+        cart_items = Cart.query.filter(Cart.user_id==user_id).all()
+        if not cart_items:
+            return {"message":EMPTY_MSG},404
+        total=0
+        before_price=0
+        data=request.get_json()
+        if 'payment_method' in data:
+            payment_method=data['payment_method']
+        else:
+            payment_method=None
+        for item in cart_items:
+            product_id = item.product_id
+            quantity = item.quantity
+            product = requests.get(PRODUCT_SERVICE_URL + str(product_id)).json()
+            if product['quantity'] < quantity:
+                return{"Message":f"Product {product['product_name']} is out of stock "},404
+            price = quantity*product['price']
+            before_price +=price
+            if price>1000 and price<2500:
+                price=price-(price*Config.discounts["DISCOUNT5"])
+            if price>2500:
+                price=price-(price*Config.discounts["DISCOUNT10"])
+            total+=price
+            item = Payment(user_id=user_id,product_id=product_id,quantity=quantity,price=price,payment_method=payment_method)
+            db.session.add(item)
+            db.session.commit()
+            requests.put(PRODUCT_SERVICE_URL+'quantity/' + str(quantity)+"/"+str(product_id))
+        requests.delete(PRODUCT_SERVICE_URL + str(user_id))
+        return {"message":f"Transaction successful, Total amount = {before_price}, After discount ={total}"}
+    except:
+        return {"message":"Some error occured"},409
